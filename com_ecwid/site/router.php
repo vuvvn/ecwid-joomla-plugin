cf0ba9f78074ac3f274512ab04167fc75e0549d0
Vladimir Tarutko <marvin@ecwid.com>
2017-05-03 12:55:10 +0400

    Backward compatibility

--- com_ecwid/site/router.php
+++ com_ecwid/site/router.php
@@ -1,38 +1,48 @@
 <?php
 
-class EcwidRouter extends JComponentRouterBase {
-	public function parse(&$segments) {
-		    $vars = array();
-		    switch($segments[0])
-		    {
-			case 'categories':
-			    $vars['view'] = 'categories';
-			    break;
-			case 'category':
-			    $vars['view'] = 'category';
-			    $id = explode(':', $segments[1]);
-			    $vars['id'] = (int)$id[0];
-			    break;
-			case 'article':
-			    $vars['view'] = 'article';
-			    $id = explode(':', $segments[1]);
-			    $vars['id'] = (int)$id[0];
-			    break;
-		    }
-		    return $vars;
+if (class_exists('JComponentRouterBase')) {
+	class EcwidRouter extends JComponentRouterBase {
+		public function parse(&$segments) {
+			return EcwidParseRoute($segments);
+		}
+
+		public function build(&$query) {
+			return EcwidBuildRoute($query);
+		}
 	}
-	
-	public function build(&$query) {
-	    $segments = array();
-	    if(isset($query['view']))
-	    {
+}
+
+function EcwidBuildRoute(&$query) {
+	$segments = array();
+	if(isset($query['view']))
+	{
 		unset($query['view']);
-	    }
-	    if(isset($query['id']))
-	    {
+	}
+	if(isset($query['id']))
+	{
 		$segments[] = $query['id'];
 		unset($query['id']);
-	    };
-	    return $segments;
+	};
+	return $segments;
+}
+
+function EcwidParseRoute(&$segments) {
+	$vars = array();
+	switch($segments[0])
+	{
+		case 'categories':
+			$vars['view'] = 'categories';
+			break;
+		case 'category':
+			$vars['view'] = 'category';
+			$id = explode(':', $segments[1]);
+			$vars['id'] = (int)$id[0];
+			break;
+		case 'article':
+			$vars['view'] = 'article';
+			$id = explode(':', $segments[1]);
+			$vars['id'] = (int)$id[0];
+			break;
 	}
+	return $vars;
 }
